e5aa44064bd59fc3bd9807bc36f7672003c98109
genevieve-exe <genevievesandra2005@gmail.com>
2025-06-26 15:00:20 +0200

    Forecast added to JS.

--- src/index.js
+++ src/index.js
@@ -62,7 +62,30 @@ function searchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function displayForecast() {
+    let forecast = document.querySelector("#forecast");
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+    
+    days.forEach(function(day) {
+
+    forecastHtml = forecastHtml +`
+    <div class="weather-forecast-day">
+                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-icon">🌑</div>
+                <div class="weather-forecast-temperatures">
+                    <div class="weather-forecast-temperature"> <strong>18°</strong></div>
+                    <div class="weather-forecast-temperature"> 7°</div>
+                </div>
+            </div>
+`;
+    });
+
+    forecast.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
 
 searchCity("Tokyo");
+displayForecast();
